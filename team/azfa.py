ba6993d7261931516c83d6358dcdba74cc76a58e
azfagtg <azfagtg@gmail.com>
2025-02-20 10:31:12 +0700

    Add files via upload

---
+++ team/azfa.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Azfa Yashifa Ramadhan"
+    nim = "2213010389"
+    peran = "mengerjakan tampilan dashboard"
+    perkenalan(nama, nim, peran)
