ae2c73dc5d7547ad3ccd900ce9caa9c9a1b774f6
erwinardn <erwinardian666@gmail.com>
2025-02-20 12:14:41 +0700

    Add files via upload

---
+++ team/erwin.py
@@ -0,0 +1,10 @@
+def perkenalan(nama, nim, peran):
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM : ", nim)
+    print("Peran saya yaitu : ", peran)
+
+if __name__ == "__main__":
+    nama = "Erwin Ardian Nugraha"
+    nim = "2213010395"
+    peran = "Mengerjakan tampilan login,tampilan registrasi "
+    perkenalan(nama, nim, peran)
